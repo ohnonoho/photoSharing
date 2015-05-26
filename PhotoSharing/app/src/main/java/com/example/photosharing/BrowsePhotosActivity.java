d4ee41fb5e8b9cbdcec1247ed2fcc0ff63da6067
ohnonoho <ohnonoho@163.com>
2015-05-25 20:17:38 -0700

    browse photos done, now try to update customed grid view

--- PhotoSharing/app/src/main/java/com/example/photosharing/BrowsePhotosActivity.java
+++ PhotoSharing/app/src/main/java/com/example/photosharing/BrowsePhotosActivity.java
@@ -1,16 +1,32 @@
 package com.example.photosharing;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 public class BrowsePhotosActivity extends ActionBarActivity {
-
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +34,34 @@ public class BrowsePhotosActivity extends ActionBarActivity {
         Intent intent = getIntent();
         //TextView tv = (TextView)findViewById(R.id.tmp);
         //tv.setText(intent.getStringExtra("deviceName"));
-        // use the device name to retrive photos from the other device
 
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ImageItem item = (ImageItem) parent.getItemAtPosition(position);
+                //Create intent
+                Intent intent = new Intent(BrowsePhotosActivity.this, PhotoDetailsActivity.class);
+                intent.putExtra("title", item.getTitle());
+                intent.putExtra("image", item.getImage());
+
+                //Start details activity
+                startActivity(intent);
+            }
+        });
+    }
+
+    private ArrayList<ImageItem> getData() {
+        // use the device name to retrive photos from the other device
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
     }
 
 
@@ -51,6 +93,76 @@ public class BrowsePhotosActivity extends ActionBarActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //this is the adapter used to fit the grid view
+    //Adapter is acts as a bridge between data source and adapter views such as ListView, GridView.
+    //Adapter iterates through the data set from beginning till the end and generate Views for each item in the list.
+    public class GridViewAdapter extends ArrayAdapter {
+        private Context context;
+        private int layoutResourceId;
+        private ArrayList data = new ArrayList();
+
+        public GridViewAdapter(Context context, int layoutResourceId, ArrayList data) {
+            super(context, layoutResourceId, data);
+            this.layoutResourceId = layoutResourceId;
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            ViewHolder holder = null;
+
+            if (row == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                row = inflater.inflate(layoutResourceId, parent, false);
+                holder = new ViewHolder();
+                holder.imageTitle = (TextView) row.findViewById(R.id.text);
+                holder.image = (ImageView) row.findViewById(R.id.image);
+                row.setTag(holder);
+            } else {
+                holder = (ViewHolder) row.getTag();
+            }
+
+            ImageItem item = (ImageItem)data.get(position);
+            holder.imageTitle.setText(item.getTitle());
+            holder.image.setImageBitmap(item.getImage());
+            return row;
+        }
+
+        public class ViewHolder {
+            TextView imageTitle;
+            ImageView image;
+        }
+    }
+
+    public class ImageItem {
+        private Bitmap image;
+        private String title;
+
+        public ImageItem(Bitmap image, String title) {
+            super();
+            this.image = image;
+            this.title = title;
+        }
+
+        public Bitmap getImage() {
+            return image;
+        }
+
+        public void setImage(Bitmap image) {
+            this.image = image;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
         }
     }
 }
---
+++ PhotoSharing/app/src/main/java/com/example/photosharing/PhotoDetailsActivity.java
@@ -0,0 +1,51 @@
+package com.example.photosharing;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class PhotoDetailsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_details);
+
+        String title = getIntent().getStringExtra("title");
+        Bitmap bitmap = getIntent().getParcelableExtra("image");
+
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(title);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageBitmap(bitmap);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photo_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
